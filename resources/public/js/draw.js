80e86856fc34ea4deccbdc2107d4e49c463086bf
Mikael Puhakka <mike@fwwm.us>
2014-11-28 20:22:15 +0200

    Clean code; style interface

--- resources/public/js/draw.js
+++ resources/public/js/draw.js
@@ -1,5 +1,5 @@
 function draw_cell(cell) {
-    var ctx = document.getElementById('T').getContext('2d');
+    var ctx = document.getElementById('C').getContext('2d');
     ctx.fillStyle = cell.bg || "#fff";
     ctx.fillRect(cell.x * BLOCKSIZE, cell.y * BLOCKSIZE, BLOCKSIZE, BLOCKSIZE);
     return;
