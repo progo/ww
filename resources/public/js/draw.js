fb92edc797cf9a8596235906a47396753b5dee14
Mikael Puhakka <mike@fwwm.us>
2014-12-05 20:11:12 +0200

    Clean up draw

--- resources/public/js/draw.js
+++ resources/public/js/draw.js
@@ -2,7 +2,7 @@ function draw_cell(cell, index) {
     var bs = globals.blocksize;
     var ctx = document.getElementById('C').getContext('2d');
 
-    var x = index       % globals.width;
+    var x = index % globals.width;
     var y = Math.floor(index / globals.width);
 
     ctx.fillStyle = cell || "#fff";
@@ -10,9 +10,4 @@ function draw_cell(cell, index) {
 
     // ctx.fillStyle = "#000";
     // ctx.font = "7pt Arial";
-    // ctx.fillText("x:" + x,      x * bs + 1,     y * bs + (bs/2));
-    // ctx.fillText("y:" + y,      x * bs + 1,     y * bs);
-    // ctx.fillText("" + index,    x * bs + 5,     y * bs + (bs/2));
-    // ctx.fillText("" + x + ", " + y,x * bs + 1,     (y+.7) * bs);
-
 }
