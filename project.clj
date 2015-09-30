57ad8a88933335e843498aeed8f41e828e2b5294
Mikael Puhakka <mike@fwwm.us>
2015-09-30 20:21:38 +0300

    Bump to clojure 1.7
    Zero problems on the way.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [ring/ring-json "0.2.0"]
                  [http-kit "2.0.0"]
                  [ring/ring-devel "1.1.8"]
